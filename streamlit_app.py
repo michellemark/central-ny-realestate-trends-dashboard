354437caa3bf1999b9ebb8605fe2da22416dda44
Michelle Mark <web-developer@michellemark.me>
2025-03-11 19:03:15 -0400

    now that etl pipeline is storing a human readable description for the value also select the property_category to display

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,10 @@ def get_cny_data_df():
 
             # Select all columns from properties and ny_property_assessments joined on property id
             query = """
-                SELECT 
-                    p.id, p.county_name, p.school_district_name, p.address_street, p.municipality_name, p.address_state, p.address_zip, 
-                    nypa.roll_year, nypa.property_class_description, nypa.full_market_value, nypa.assessment_land, nypa.assessment_total 
+                SELECT  
+                    p.id, p.county_name, p.school_district_name, p.address_street, p.municipality_name, p.address_state,
+                    p.address_zip, nypa.roll_year, nypa.property_category, nypa.property_class_description, 
+                    nypa.full_market_value, nypa.assessment_land, nypa.assessment_total 
                 FROM 
                     properties p
                 INNER JOIN 
