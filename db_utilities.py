1402ed2e71a44b9c592356027114a30998454029
Michelle Mark <web-developer@michellemark.me>
2025-04-02 18:00:34 -0400

    remove change of id column type, this is text

--- db_utilities.py
+++ db_utilities.py
@@ -110,7 +110,6 @@ def get_cny_data_df():
 
             # Optimize Data Types to Reduce Memory Usage
             # Convert numeric columns to smaller, efficient types
-            df["id"] = df["id"].astype("int32")
             df["roll_year"] = df["roll_year"].astype("int16")
             df["full_market_value"] = df["full_market_value"].astype("float32")
             df["front"] = df["front"].astype("float32")
